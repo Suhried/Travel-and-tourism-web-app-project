68c2834bed0de7663655743cb963ddace5aed6e9
Suhried <shantorad@gmail.com>
2021-02-22 21:53:22 +0600

    task 4

--- lab4/task_4.php
+++ lab4/task_4.php
@@ -89,10 +89,9 @@
             echo "Password: ".$_POST["pass"]."<br>";
             echo "Gender: ".$_POST["gender"]."<br>";
             echo "Profession: ".$_POST["profession"]."<br>";
-            echo "Biography: ".$_POST["bio"]."<br>";
+            echo "Bio: ".$_POST["bio"]."<br>";
             echo "Date: ".$_POST["date"]."<br>";
             echo "E-Mail: ".$_POST["email"]."<br>";
-            //echo "Salary: ".$_POST["salary"]."<br>";
             $var = $_POST["Hobbies"];
             echo "Hobbies: ";
             count($var);
@@ -137,9 +136,60 @@
 				</tr>
 				<tr>
 					<td><span><b>Email</b></span></td>
-					<td>:<input type="password" name="pass" value="<?php echo $pass;?>">
+					<td>:<input type="email" name="email" value="<?php echo $email;?>">
 					<span><?php echo $err_pass;?></span></td>
 				</tr>
+				<tr>
+					<td><span><b>Phone</b></span></td>
+					<td>:<input type="tel" name="phone_no" size="4"> - <input type="tel" name="phone_no" size= 9></td>
+				</tr>
+				<tr>
+					<td><span><b>Address</b></span></td>
+					<td>:<input type="street" name="street"></br></td>
+				</tr>
+				<tr>
+					<td><span><b></b></span></td>
+					<td><input type="city" name="city" size="4"> - <input type="state" name="state" size= 9></td>
+				</tr>
+				<tr>
+					<td><span><b></b></span></td>
+					<td><input type="zip" name="zip">
+				</tr>
+				<tr>
+				<td><span><b>Birth Date</b></span></td>
+				
+				<td>
+				<select name="day">
+					<option disabled selected>Day</option>
+					<?php
+						for($i=1;$i<=31;$i++)
+						{
+							echo "<option>$i</option>";
+						}
+					?>
+					</select>
+					<select name="month">
+					<option disabled selected>Month</option>
+					<?php
+						$mon= array("January","February","March","April","May","June","July","August","September","October","November","December");
+						for($j=0;$j<count($mon);$j++)
+						{
+							echo "<option>$mon[$j]</option>";
+						}
+					?>
+				</select>
+				<select name="year">
+					<option disabled selected>Year</option>
+					<?php
+						for($k=1900;$k<=2021;$k++)
+						{
+							echo "<option>$k</option>";
+						}
+					?>
+				</select>
+				</td>
+				</tr>
+
 				<tr>
 					<td><span><b>Gender</b></span></td>
 					<td>:<input type="radio" name="gender" value="Male"><span>Male</span>
@@ -147,45 +197,23 @@
 						<span><?php echo "&nbsp ".$err_gender;?></span></td>
 				</tr>
 				<tr>
-					<td><span><b>Where did you hear about us?</b></span></td>
-					<td>:<input type="checkbox" name="about[]" value="A friend or colleges"><span>Movies</span>
-					    <input type="checkbox" name="about[]" value="Google"><span>Music</span>
-						<input type="checkbox" name="about[]" value="Blog Post"><span>Games</span>
-						<input type="checkbox" name="about[]" value="News Article"><span>Sports</span>
+					<td><span><b>Where did you</br> hear about us?</b></span></td>
+					<td><input type="checkbox" name="about[]" value="friend"><span>A Friend or Colleague</br></span>
+					    <input type="checkbox" name="about[]" value="Google"><span>Google</br></span>
+						<input type="checkbox" name="about[]" value="Blog_Post"><span>Blog Post</br></span>
+						<input type="checkbox" name="about[]" value="News_Article"><span>News Article</br></span>
 						<span><?php echo "&nbsp  ".$err_about;?></span></td>
 				</tr>
-				<tr>
-					<td><span><b>Birth Date</b></span></td>
-					<td>: 
-						<select name="Birth Date">
-							<option disabled selected>date</option>
-							<?php
-							for($i=1;$i<=31;++$i)
-							{
-								echo "<option>$i</option>";
-							}
-							?>
-						</select>
-						<span><?php echo "&nbsp".$err_profession;?></span>
-					</td>
-				</tr>
+				
 				<tr>
 	 				<td><span><b>Bio</b></span></td>
 					 <td>:<textarea name="bio" value="<?php echo $bio;?>"></textarea>
 					 <span><?php echo "&nbsp".$err_bio;?></span></td>
 				</tr>
 				
-				<tr>
-					<td><span><b>Mobile/Phone</b></span></td>
-					<td>:<input type="tel" name="phone_no"></td>
-				</tr>
-				<tr>
-					<td><span><b>Date</b></span></td>
-					<td>:<input type="date" name="date"></td>
-				</tr>
 			</table>
 			<br>
-			<input type="submit" name="submit" value="submit">
+			<input type="submit" name="submit" value="register" >
 		</form>
 	</body>
 
